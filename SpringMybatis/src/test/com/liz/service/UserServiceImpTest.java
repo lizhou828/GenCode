49d7048aaedbbfd57aef73781d7f3e1b7d1acb9a
lizhou828 <lizhou828@126.com>
2017-07-23 16:53:34 +0800

    BaseUnitTest

--- SpringMybatis/src/test/com/liz/service/UserServiceImpTest.java
+++ SpringMybatis/src/test/com/liz/service/UserServiceImpTest.java
@@ -21,14 +21,8 @@ import java.util.List;
  * Created by lizhou on 2017年04月24日 22时34分
  */
 
-// 告诉spring怎样执行
-@RunWith(SpringJUnit4ClassRunner.class)
-//  标明是web应用测试
-@WebAppConfiguration(value = "src/main/webapp") //可以不填，默认此目录
-// 配置文件地址
-@ContextConfiguration(locations = { "file:src/main/resources/applicationContext.xml", "file:src/main/resources/spring-mybatis.xml", "file:src/main/resources/springMVC-servlet.xml" })
 
-public class UserServiceImpTest {
+public class UserServiceImpTest  extends BaseUnitTest{
     @Autowired
     private UserMapper userMapper;
 
